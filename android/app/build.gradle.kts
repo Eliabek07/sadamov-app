6fb38bbfb1884adeeb3fc5cb1d85af851f5a57a5
Eliabeks07 <eliabeeks@gmail.com>
2025-12-05 09:38:34 -0300

    chore: adiciona google-services.json ao .gitignore por segurança

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -35,4 +36,12 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+    
+    // Firebase App Distribution (opcional, mas recomendado para distribuição)
+    // implementation("com.google.firebase:firebase-appdistribution-api-ktx")
 }
